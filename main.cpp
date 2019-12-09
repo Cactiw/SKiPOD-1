64731954cf879fdbfe004b93b97599a47f3a60f0
cactiw <knyazev1514@gmail.com>
2019-12-09 13:16:49 +0300

    Execution is now repeated several times

--- main.cpp
+++ main.cpp
@@ -57,14 +57,14 @@ int main() {
     }
     #pragma omp barrier
     for (int i = 0; i < NUMBER_THREADS; ++i) {
+        omp_set_num_threads(NUM_THREADS[i]);
+        double start_time = omp_get_wtime();
+        std::cout << "Matrix generated. Counting rank with " << NUM_THREADS[i] << " threads ..." << std::endl;
+        //std::cout << rank(a, n, m);
+        std::cout << rank(reinterpret_cast<int **>(reinterpret_cast<int *>(a)), m, n) << std::endl;
+        std::cout << "Computation took " << omp_get_wtime() - start_time << " seconds to complete" << std::endl;
 
     }
-    double start_time = omp_get_wtime();
-    std::cout << "Matrix generated. Counting rank..." << std::endl;
-    //std::cout << rank(a, n, m);
-    std::cout << rank(reinterpret_cast<int **>(reinterpret_cast<int *>(a)), m, n) << std::endl;
-    std::cout << "Computation took " << omp_get_wtime() - start_time << " seconds to complete" << std::endl;
-
 
     return 0;
 }
